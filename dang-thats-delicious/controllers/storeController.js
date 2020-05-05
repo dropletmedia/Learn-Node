f434bb07e1e904fc329d61d1ee808179df135d9b
dropletmedia <phil@dropletmedia.co.uk>
2020-05-05 19:58:40 +0100

    end vid 13

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -18,3 +18,9 @@ exports.createStore = async (req, res) => {
   );
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. query the database for store list
+  const stores = await Store.find();
+  res.render("stores", { title: "Stores", stores });
+};
